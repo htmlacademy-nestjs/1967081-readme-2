b0ed6c8bd451c030ab0d39678475a9ff1ebe1fdb
Cr4zyGleb <Cr4zyGleb@gmail.com>
2022-12-11 04:00:42 +0300

    Урок 3

--- readme/apps/users/src/app/app.module.ts
+++ readme/apps/users/src/app/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
+import envSchema from './env.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoDbConfig } from '../config/mongodb.config';
 
 @Module({
-  imports: [BlogUserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+      validationSchema: envSchema
+    }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
+    AuthModule,
+    BlogUserModule
+  ],
   controllers: [],
   providers: [],
 })
